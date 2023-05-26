1a874f400b37cef8addc2c62692a6d1a76dc1b4a
DTK <cristhiandanielcoaquira@gmail.com>
2023-05-26 09:16:36 -0400

    Implement Splash Screen

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
-<script setup>
-import Home from '@/components/Home.vue'
+<template>
+  <Suspense>
+    <template #default>
+      <Home></Home>
+    </template>
+    <template #fallback>
+      <SplashScreen></SplashScreen>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import SplashScreen from '@/components/SplashScreen.vue'
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    SplashScreen,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import('@/components/Home.vue'))
+          }, 500)
+        })
+    )
+  }
+}
 </script>
 
-<template>
-  <Home></Home>
-</template>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
